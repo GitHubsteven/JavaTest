680b4482f7391f1fd967ad6cbfaad1712385bf96
rongbin.xie <rongbin.xie@voyageone.cn>
2017-11-09 11:13:36 +0800

    before reverse 2

---
+++ src/com/java/reversetest/ClassRealToDel.java
@@ -0,0 +1,10 @@
+package com.java.reversetest;
+
+/**
+ * Created by rongbin.xie on 2017/11/9.
+ */
+public class ClassRealToDel {
+    public static void main(String[] args) {
+        System.out.println("hello,world");
+    }
+}
